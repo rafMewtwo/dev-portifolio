9ba4c3bc70b669acef6bd8bb14908d1e5a266f98
rafMewtwo <ponciano.raf@gmail.com>
2021-02-28 15:51:31 -0300

    lets gox3

---
+++ build/precache-manifest.7db162a7913e7cf0ca2f9aae73570116.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ec181c73b7e0f8adca29753a897594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09a007cf84a335f9acee",
+    "url": "/static/css/main.daaf0861.chunk.css"
+  },
+  {
+    "revision": "b6544d5359ba768d339a",
+    "url": "/static/js/2.7767e3eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7767e3eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a007cf84a335f9acee",
+    "url": "/static/js/main.dc6636b3.chunk.js"
+  },
+  {
+    "revision": "84e5482291f212a94bb1",
+    "url": "/static/js/runtime-main.c7e298dc.js"
+  },
+  {
+    "revision": "2f47c78fe98cf5a9b9bb65252f5f0c71",
+    "url": "/static/media/background4.2f47c78f.jpg"
+  }
+]);
